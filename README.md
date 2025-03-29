83b959eee47601e1c0a6af56afc056341165ba20
wilbert <a21311056@uthermosillo.edu.mx>
2025-03-29 05:32:43 -0700

    creación del modelo, controlador, factories y migración. instalación de fontawesome y boostrap. creacion de las migraciones junto con el seed

--- README.md
+++ README.md
@@ -64,3 +64,9 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+# .
+## Autor
+- Nombre: Wilbert Jesus Rubi Garcia.  
+- Email: a21311056@uthermosillo.edu.mx 
+- GitHub: [@wilbert-R](https://github.com/wilbert-R)
---
+++ database/migrations/2025_03_29_115739_create_games_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',80);
+            $table->integer('levels');
+            $table->date('release');
+            $table->string('image',80);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
